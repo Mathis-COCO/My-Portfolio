08bb12d736c7ad6d6bc5fc13ef01f99956f53860
Mathis-COCO <mathis.coco@ynov.com>
2023-05-30 18:51:50 +0200

    :wrench: added moon and transparent background - working on landing page text

--- src/components/ThreeArt.jsx
+++ src/components/ThreeArt.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from 'react';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import earthTexture from '../assets/img/earth_texture.jpg';
+import moonTexture from '../assets/img/moon_texture.jpg';
 
 const ThreeBuilding = () => {
   const mountRef = useRef(null);
@@ -9,26 +10,39 @@ const ThreeBuilding = () => {
   useEffect(() => {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer( { alpha: true } );
 
     renderer.setSize(window.innerWidth, window.innerHeight);
     mountRef.current.appendChild(renderer.domElement);
 
-    const geometry = new THREE.SphereGeometry(1, 32, 32);
+    // Sphère principale (Terre)
+    const mainSphereGeometry = new THREE.SphereGeometry(1, 32, 32);
     const textureLoader = new THREE.TextureLoader();
-    const texture = textureLoader.load(earthTexture);
-    const material = new THREE.MeshBasicMaterial({ map: texture });
-    const sphere = new THREE.Mesh(geometry, material);
+    const earth_texture = textureLoader.load(earthTexture);
+    const mainSphereMaterial = new THREE.MeshBasicMaterial({ map: earth_texture });
+    const mainSphere = new THREE.Mesh(mainSphereGeometry, mainSphereMaterial);
+    scene.add(mainSphere);
 
-    scene.add(sphere);
+    // Sphère secondaire (Lune)
+    const secondarySphereGeometry = new THREE.SphereGeometry(0.3, 16, 16);
+    const moon_texture = textureLoader.load(moonTexture);
+    const secondarySphereMaterial = new THREE.MeshBasicMaterial({ map: moon_texture });
+    const secondarySphere = new THREE.Mesh(secondarySphereGeometry, secondarySphereMaterial);
+    mainSphere.add(secondarySphere);
+
+    // Position de la sphère secondaire par rapport à la sphère principale
+    secondarySphere.position.x = 2; // Modifier les valeurs x, y, z pour ajuster la position
 
     camera.position.z = 5;
 
     const controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableZoom = true; // Activer le zoom
+    controls.enableZoom = true;
 
     function animate() {
       requestAnimationFrame(animate);
+
+      mainSphere.rotation.y += 0.001;
+      secondarySphere.rotation.y += 0.003;
 
       renderer.render(scene, camera);
     }
