dc19b0d6d4df984b3ad23c2eac01cd1dbb34c534
Mathis-COCO <mathis.coco@ynov.com>
2023-05-30 17:51:46 +0200

    :wrench: possibility to zoom into earth

--- src/components/ThreeArt.jsx
+++ src/components/ThreeArt.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import * as THREE from 'three';
-import earthTexture from '../assets/img/earth_texture.jpg'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import earthTexture from '../assets/img/earth_texture.jpg';
 
 const ThreeBuilding = () => {
   const mountRef = useRef(null);
@@ -9,24 +10,25 @@ const ThreeBuilding = () => {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer();
-    
+
     renderer.setSize(window.innerWidth, window.innerHeight);
     mountRef.current.appendChild(renderer.domElement);
-    
+
     const geometry = new THREE.SphereGeometry(1, 32, 32);
     const textureLoader = new THREE.TextureLoader();
     const texture = textureLoader.load(earthTexture);
     const material = new THREE.MeshBasicMaterial({ map: texture });
     const sphere = new THREE.Mesh(geometry, material);
-    
+
     scene.add(sphere);
-    
+
     camera.position.z = 5;
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableZoom = true; // Activer le zoom
 
     function animate() {
       requestAnimationFrame(animate);
-
-      sphere.rotation.y += 0.005;
 
       renderer.render(scene, camera);
     }
