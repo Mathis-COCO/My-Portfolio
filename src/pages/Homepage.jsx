f6ec0f1488126fd9060d04f0cbbd2d8d7af8247d
Mathis-COCO <mathis.coco@ynov.com>
2023-05-30 17:09:14 +0200

    ➕ added threejs with 3D earth for testing purposes

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import '../assets/css/Homepage.scss'
 import Navbar from '../components/Navbar';
+import ThreeBuilding from '../components/ThreeArt';
 
 function Homepage() {
   return (
     <div>
         <Navbar />
         <p>HOMEPAGE</p>
+        <ThreeBuilding />
     </div>
   );
 }
